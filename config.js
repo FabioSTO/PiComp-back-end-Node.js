904740a509c1006691615b10476c498c9f2ebf27
FabioSTO <f.torrado@udc.es>
2023-09-25 17:51:30 +0200

    Configuring AWS

--- config.js
+++ config.js
@@ -8,4 +8,9 @@ module.exports = {
     password: process.env.DB_PASSWORD || "<contraseña>",
     database: process.env.DB_DATABASE || "<database>",
   },
+  awsConfig: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "<accessKey de AWS>",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "<secretAccessKey de AWS>",
+    region: process.env.REGION || "<bucketRegion>",
+  }
 };
