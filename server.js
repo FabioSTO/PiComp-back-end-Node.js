5cfe29f7949c3e92c93087e0437ea477a786e0cb
FabioSTO <f.torrado@udc.es>
2023-09-25 20:54:35 +0200

    Functional bucket submissions

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express"); // Esto es common.js (modules y eso)
 const config = require("./config");   // Esto es common.js (modules y eso)
 const app = express ();
 const cors = require("cors"); // Permitir que solicitudes crucen distintos orígenes
+const bodyParser = require('body-parser');
 
 // Conexión a Express
+
+app.use(bodyParser.json({ limit: '10mb' })); // Para permitir fotos más grandes
 
 app.use(express.json()); 
 
