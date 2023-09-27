da85560976005759ac1c482e830b15a55cd8ab39
FabioSTO <f.torrado@udc.es>
2023-09-28 01:20:23 +0200

    Moved S3 functions to separate file

---
+++ s3Utils.js
@@ -0,0 +1,89 @@
+const s3 = require('./awsConfig');
+
+function uploadImageToS3(userID, imageName, imageComp, callback) { // Subir imagen al bucket
+  
+  const imageExtension = imageComp.split('/')[1].split(';')[0]; // Obtiene la extensión de la imagen a partir de la base64
+  const imageDecoded = Buffer.from(imageComp.split(',')[1], 'base64'); // Decodifica la imagen Base64
+  const imageNameUnderscored = imageName.replaceAll(" ", "_"); // Cambia los espacios por "_" para formalizar el nombre de la foto
+
+  const params = {
+    Bucket: 'picomp-bucket',
+    Key: `${userID}_${imageNameUnderscored}.${imageExtension}`, 
+    ContentType: 'image/*',
+    Body: imageDecoded,
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      callback(err,null);
+    } else {
+      console.log('Imagen cargada exitosamente:', data.Location);
+      callback(null, data.Location); // Envía la URL de la imagen cargada
+    }
+  });
+}
+
+function uploadProfilePicToS3(name, profilePic, callback) { // Subir profilePic al bucket
+  
+  const imageExtension = profilePic.split('/')[1].split(';')[0]; // Obtiene la extensión de la imagen a partir de la base64
+  const imageDecoded = Buffer.from(profilePic.split(',')[1], 'base64'); // Decodifica la imagen Base64
+
+  const params = {
+    Bucket: 'picomp-bucket',
+    Key: `${name}_profilePic.${imageExtension}`, 
+    ContentType: 'image/*',
+    Body: imageDecoded, // Decodifica la imagen Base64
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      callback(err, null);
+    } else {
+      console.log('Imagen cargada exitosamente:', data.Location);
+      callback(null, data.Location); // Envía la URL de la imagen cargada
+    }
+  });
+}
+
+
+// Obtener URL temporal de la imagen para mostrarla en el login
+
+function getProfilePicFromS3(keyImageUrl) { 
+
+  const params = {
+    Bucket: 'picomp-bucket',
+    Key: `${keyImageUrl}`,
+  };
+
+  const presignedUrl = s3.getSignedUrl('getObject', params);
+  console.log(presignedUrl);
+
+  return presignedUrl;
+}
+
+function getUserID(username, callback) {
+  const findIDQuery = "SELECT id FROM users WHERE username = ?"
+
+  con.query(findIDQuery, [username], (err, results) => {
+    if (err) {
+      console.error(err);
+      callback(err, null); // Pasas error al callback
+    } else {
+      if (results.length > 0) {
+        const userID = results[0].id;
+        callback(null, userID); // Pasas el userID al callback
+      } else {
+        callback(new Error("Usuario no encontrado"), null);
+      }
+    }
+  });
+}
+
+module.exports = {
+  uploadImageToS3,
+  uploadProfilePicToS3,
+  getProfilePicFromS3,
+  getUserID,
+};
--- userRoutes.js
+++ userRoutes.js
@@ -1,74 +1,15 @@
-// userRoutes.js
 const express = require("express");
 const router = express.Router();
 const con = require("./db");
 const { hashPassword, comparePasswords } = require("./bcryptUtils");
-//const AWS = require('aws-sdk');
-const s3 = require('./awsConfig');
+const s3Utils = require('./s3Utils');
 
-function uploadImageToS3(userID, imageName, imageComp, callback) { // Subir imagen al bucket
-  
-  const imageExtension = imageComp.split('/')[1].split(';')[0]; // Obtiene la extensión de la imagen a partir de la base64
-  const imageDecoded = Buffer.from(imageComp.split(',')[1], 'base64'); // Decodifica la imagen Base64
-  const imageNameUnderscored = imageName.replaceAll(" ", "_"); // Cambia los espacios por "_" para formalizar el nombre de la foto
+// Muestra imágenes para votar
+router.post("/showImagesToVote", (request, response) => {
+  const {  } = request.body;
+});
 
-  const params = {
-    Bucket: 'picomp-bucket',
-    Key: `${userID}_${imageNameUnderscored}.${imageExtension}`, 
-    ContentType: 'image/*',
-    Body: imageDecoded,
-  };
-
-  s3.upload(params, (err, data) => {
-    if (err) {
-      console.error(err);
-      callback(err,null);
-    } else {
-      console.log('Imagen cargada exitosamente:', data.Location);
-      callback(null, data.Location); // Envía la URL de la imagen cargada
-    }
-  });
-}
-
-function uploadProfilePicToS3(name, profilePic, callback) { // Subir profilePic al bucket
-  
-  const imageExtension = profilePic.split('/')[1].split(';')[0]; // Obtiene la extensión de la imagen a partir de la base64
-  const imageDecoded = Buffer.from(profilePic.split(',')[1], 'base64'); // Decodifica la imagen Base64
-
-  const params = {
-    Bucket: 'picomp-bucket',
-    Key: `${name}_profilePic.${imageExtension}`, 
-    ContentType: 'image/*',
-    Body: imageDecoded, // Decodifica la imagen Base64
-  };
-
-  s3.upload(params, (err, data) => {
-    if (err) {
-      console.error(err);
-      callback(err, null);
-    } else {
-      console.log('Imagen cargada exitosamente:', data.Location);
-      callback(null, data.Location); // Envía la URL de la imagen cargada
-    }
-  });
-}
-
-
-// Obtener URL temporal de la imagen para mostrarla en el login
-
-function getProfilePicFromS3(keyImageUrl) { 
-
-  const params = {
-    Bucket: 'picomp-bucket',
-    Key: `${keyImageUrl}`,
-  };
-
-  const presignedUrl = s3.getSignedUrl('getObject', params);
-  console.log(presignedUrl);
-
-  return presignedUrl;
-}
-
+// Registro de nuevo usuario
 router.post("/registerAccount", (request, response) => {
   const { name, email, password, profilePic } = request.body;
 
@@ -78,7 +19,7 @@ router.post("/registerAccount", (request, response) => {
       console.error(err);
       response.status(500).json({ message: "Error al registrar el usuario" });
     } else {
-      uploadProfilePicToS3(name, profilePic, (err, imageUrl) => {
+      s3Utils.uploadProfilePicToS3(name, profilePic, (err, imageUrl) => {
         if (err) {
           console.error(err);
           response.status(500).json({ message: "Error al cargar la imagen en S3" });
@@ -103,12 +44,13 @@ router.post("/registerAccount", (request, response) => {
   });
 });
 
+
 // Inicio de sesión de usuario
 router.post("/loginAccount", (request, response) => {
   const { email, password } = request.body;
 
   // Obtener datos de usuario por correo electrónico
-  const findUserQuery = "SELECT username, password, profilePic FROM users WHERE email = ?";
+  const findUserQuery = "SELECT username, password, profilePic, email, submitted, voted FROM users WHERE email = ?";
   con.query(findUserQuery, [email], (err, results) => {
     if (err) {
       console.error(err);
@@ -124,11 +66,11 @@ router.post("/loginAccount", (request, response) => {
           console.error(compareErr);
           response.status(500).json({ message: "Error al comparar contraseñas" });
         } else if (passwordsMatch) {
-          const { username, profilePic } = results[0];
+          const { username, profilePic, email, submitted, voted } = results[0];
           
-          const presignedUrl = getProfilePicFromS3(profilePic);
+          const presignedUrl = s3Utils.getProfilePicFromS3(profilePic);
 
-          response.status(200).json({ message: "Usuario logueado correctamente", username, profilePic: presignedUrl });
+          response.status(200).json({ message: "Usuario logueado correctamente", username, profilePic: presignedUrl, email, submitted, voted });
         } else {
           response.status(401).json({ message: "Contraseña incorrecta" });
         }
@@ -137,34 +79,17 @@ router.post("/loginAccount", (request, response) => {
   });
 });
 
-function getUserID(username, callback) {
-  const findIDQuery = "SELECT id FROM users WHERE username = ?"
-
-  con.query(findIDQuery, [username], (err, results) => {
-    if (err) {
-      console.error(err);
-      callback(err, null); // Pasas error al callback
-    } else {
-      if (results.length > 0) {
-        const userID = results[0].id;
-        callback(null, userID); // Pasas el userID al callback
-      } else {
-        callback(new Error("Usuario no encontrado"), null);
-      }
-    }
-  });
-}
-
+// Usuario submitea una imagen
 router.post("/submitImage", (request, response) => {
   const { imageName, imageComp, username } = request.body;
   const points = 0;
 
-  getUserID(username, (err, userID) => {
+  s3Utils.getUserID(username, (err, userID) => {
     if (err) {
       console.error(err);
       response.status(500).json({ message: "Error al obtener el ID de usuario" });
     } else {
-      uploadImageToS3(userID, imageName, imageComp, (err, imageUrl) => {
+      s3Utils.uploadImageToS3(userID, imageName, imageComp, (err, imageUrl) => {
         if (err) {
           console.error(err);
           response.status(500).json({ message: "Error al cargar la imagen en S3" });
